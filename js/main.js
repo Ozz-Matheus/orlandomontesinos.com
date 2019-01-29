161e08771791e91dbcb840fc83d4eeb0cb9e9b86
Orlando Montesinos <montesinos.quintana@gmail.com>
2019-01-29 16:09:11 -0600

    Update

--- js/main.js
+++ js/main.js
@@ -195,15 +195,6 @@
             }
         });
 
-        /** Blog Masonry */
-        $('.home .post-container').imagesLoaded(function() {
-            $('.home .post-container').isotope({
-                itemSelector: '.post',
-                layoutMode: 'masonry',
-                transitionDuration: '0s'
-            });
-        });
-
         /** Testimonial carousel */
         $('.testimonial-carousel').owlCarousel({
             autoplay: true,
@@ -220,75 +211,8 @@
             animateOut: 'fadeOut',
         });
 
-        /** Client carousel */
-        $('.client-carousel').owlCarousel({
-            autoplay: true,
-            autoplaySpeed: 1000,
-            autoplayTimeout: 5000,
-            autoplayHoverPause: true,
-            autoWidth: false,
-            autoHeight: false,
-            items: 3,
-            margin: 30,
-            loop: true,
-            nav: false,
-            dots: false,
-            navText: false,
-            responsive: {
-                0: {
-                    items: 2
-                },
-                600: {
-                    items: 4
-                },
-                1000: {
-                    items: 3
-                },
-                1440: {
-                    items: 4
-                },
-                1600: {
-                    items: 5
-                },
-                2000: {
-                    items: 6
-                }
-            }
-        });
-
-    });
 
 
-    /** Contact Form */
-    $('.contact-form').on('submit', function(e) {
-        e.preventDefault();
-        var name = $('#name').val();
-        var email = $('#email').val();
-        var subject = 'Contact form submitted by ' + name;
-        var message = $('#message').val();
-        var dataString = 'name=' + name + '&email=' + email + '&subject=' + subject + '&message=' + message;
-
-        function isValidEmail(emailAddress) {
-            var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-            return pattern.test(emailAddress);
-        };
-
-        if (isValidEmail(email) && (message.length > 1) && (name.length > 1)) {
-            $.ajax({
-                type: 'POST',
-                url: 'php/contact.php',
-                data: dataString,
-                success: function() {
-                    $('.success').fadeIn(1000);
-                    $('.error').fadeOut(500);
-                }
-            });
-        } else {
-            $('.error').fadeIn(1000);
-            $('.success').fadeOut(500);
-        }
-
-        return false;
     });
 
 }(jQuery));
