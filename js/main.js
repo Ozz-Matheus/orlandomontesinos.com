ae1cbf7ec95a617a823188c842fe0064c76449d3
Ozz Matheus <montesinos.quintana@gmail.com>
2019-05-08 17:44:55 -0500

    Confirm update anchors

--- js/main.js
+++ js/main.js
@@ -165,11 +165,11 @@
         });
 
         /** MagnificPopup For Portfolio */
-        $('a.lightbox').magnificPopup({
+        $('.lightbox').magnificPopup({
             type: 'image',
         });
 
-        $('a.lightbox-iframe').magnificPopup({
+        $('.lightbox-iframe').magnificPopup({
             type: 'iframe',
             iframe: {
                 markup: '<div class="mfp-iframe-scaler">' + '<div class="mfp-close"></div>' + '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' + '</div>',
